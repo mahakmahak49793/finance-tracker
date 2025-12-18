10e360ac3c3929b6b22931e9d2e0ed00bed6cad4
Mahak <mahakmahak49793@gmail.com>
2025-12-18 23:24:16 +0530

    update user api

--- app/api/user/me/route.ts
+++ app/api/user/me/route.ts
@@ -3,9 +3,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { verifyToken } from '@/lib/auth'
 import { cookies } from 'next/headers'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from '@/lib/prisma'
 
 export async function GET(request: NextRequest) {
   try {
